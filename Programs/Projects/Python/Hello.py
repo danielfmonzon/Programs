3ecc1daf315a8b9e343274a7d7c12de6347669fb
danielfmonzon <monzond@ufl.edu>
2024-03-08 22:59:08 -0500

    Resolved merge conflicts

---
+++ Programs/Projects/Python/Hello.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Program: Hello
+# Description: In this project students will write a very simple
+#              program using Python’s input and output methods. The program
+#              will prompt the user to enter responses to several questions, 
+#              displaying the answer each time. The project is
+#              designed to give students an opportunity to practice very 
+#              basic input and output via a simple project
+# Author: Daniel Monzon
+# Date: 03/08/2024
+# Version: 1.0
+
+name = input("Hello. What is your name? ")
+print(f"It's nice to meet you, {name}. ")
+
+age = input("How old are you? ")
+print(f"I see that you are still quite young at only {age}.")
+
+location = input("Where do you live? ")
+print(f"Wow! I've always wanted to go to {location}. Thanks for chatting with me. Bye!")
