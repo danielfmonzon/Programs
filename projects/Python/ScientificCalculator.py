2fad396604b075aa3f7e0cb6007cd2fe96e1a2a0
danielfmonzon <monzond@ufl.edu>
2024-03-14 18:00:24 -0400

    Updates to sci_calc.py

--- projects/Python/ScientificCalculator.py
+++ projects/Python/ScientificCalculator.py
@@ -1,5 +1,4 @@
 import math
-from tkinter.tix import InputOnly
 
 class Calculator:
     
@@ -8,7 +7,6 @@ class Calculator:
         self.menu_selection = int(menu_selection)
         self.sum_calculation = float(0.0)
         self.num_calculation = int(0.0)
-        self.avg_calculation = float(0.0)
     
     def add_function(self, input_one, input_two):
         self.result = input_one + input_two
@@ -30,16 +28,22 @@ class Calculator:
         
     def log_function(self, input_one, input_two):
         try:
-            self.result = math.log(input_one, input_two)
+            self.result = math.log(input_two, input_one)  # math.log(x, base) : log (2) base = x
         except ValueError:
             print(f"Error: Invalid selection! Input {input_one} causes Value Error.")
             return None
         
-    def avg_function(self, input_one, input_two):
-        #self.avg_calculation = self.sum_calculation / self.num_calculation
-        pass
-    
-        
+    def avg_function(self):
+        if self.sum_calculation > 0:
+            print(f"Sum of calculations: {self.sum_calculation}")
+            print(f"Number of calculations: {self.num_calculation}")
+            rounded_avg = round((self.sum_calculation / self.num_calculation), 2)
+            print(f"Average of calculations: {rounded_avg}")
+            print("")
+        else:
+           print("Error: no calculations yet to average!")
+           print("")
+
     def menu(self):
         print(f"\nCurrent Result: {self.result}\n")
         
@@ -64,22 +68,37 @@ class Calculator:
              return False
             
     def get_operands(self):
-        input_one = float(input("Enter first operand: "))
-        input_two = float(input("Enter second operand: "))
+        first_input = input("Enter first operand: ")
+        second_input = input("Enter second operand: ")
+        
+        if first_input.upper() == "RESULT":
+            input_one = self.result
+        else:
+            input_one = float(first_input)
+            
+        if second_input.upper() == "RESULT":
+            input_two = self.result
+        else:
+            input_two = float(second_input)
+            
         return input_one, input_two
 
 if __name__ == "__main__":
     
     calc = Calculator()
     loop = True
+    display_menu = True
     
     while loop:
-        calc.menu()
+        if display_menu:
+            calc.menu()
+            
         menu_selection = calc.menu_input() # Unpack and assign menu selection and 
         # operands from user input returned by calc.menu_input()
+
         if menu_selection in [1, 2, 3, 4, 5, 6]:
             input_one, input_two = calc.get_operands()
-
+            
         if menu_selection == 1:
             calc.add_function(input_one, input_two)
         elif menu_selection == 2:
@@ -93,12 +112,18 @@ if __name__ == "__main__":
         elif menu_selection == 6:
             calc.log_function(input_one, input_two)
         elif menu_selection == 7:
-            pass # Display Average calc.avg_function(input_one, input_two)
+            calc.avg_function()
+            display_menu = False
+            continue
         elif menu_selection == 0:
             print("Thanks for using this calculator. Goodbye!")
             loop = False
         else:
             print("Error: Invalid selection! Terminating program.")
             loop = False
-            
-    
+         
+        if menu_selection in [1, 2, 3, 4, 5, 6]:
+           calc.num_calculation += 1
+           calc.sum_calculation += calc.result
+           display_menu = True
+           
